cd5b53f6254e4d57b3e40db0a561d3827fe1b95a
janiopi <janio072003@hotmail.com>
2025-05-17 17:52:54 -0500

    Add: Projects component

---
+++ cv-app/src/components/Projects.tsx
@@ -0,0 +1,142 @@
+'use client';
+// This is a client component
+
+import { useState } from 'react';
+
+interface AboutMeProps {
+  data: {
+    name: string;
+    description: string;
+    github: string;
+    startDate: string;
+    endDate: string;
+  }[];
+
+  setData: (
+    data: {
+      name: string;
+      description: string;
+      github: string;
+      startDate: string;
+      endDate: string;
+    }[]
+  ) => void;
+}
+
+function Projects({ data, setData }: AboutMeProps) {
+  const [localData, setLocalData] = useState(data);
+
+  const handleInputChange =
+    (index: number, field: keyof (typeof localData)[0]) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const updatedProject = [...localData];
+      updatedProject[index] = {
+        ...updatedProject[index],
+        [field]: event.target.value,
+      };
+      setLocalData(updatedProject);
+    };
+
+  const handleSubmit = () => {
+    //Add an empty project to the list
+    const newProject = {
+      name: '',
+      description: '',
+      github: '',
+      startDate: '',
+      endDate: '',
+    };
+    const updatedLocalData = [...localData, newProject];
+    //Update the data in the parent component
+    setLocalData(updatedLocalData);
+    //Update the data in the prop
+    setData(updatedLocalData);
+  };
+
+  const handleDelete = (index: number) => {
+    const updatedLocalData = localData.filter((_, i) => i !== index);
+    setLocalData(updatedLocalData);
+    setData(updatedLocalData);
+  };
+
+  return (
+    <div className="flex flex-col space-y-4 w-96 mx-auto">
+      <label className="text-sm font-medium text-white-700 text-center">
+        {'Projects'}
+      </label>
+
+      {localData.map((project, index) => (
+        <div key={index} className="border p-4 rounded-md space-y-2">
+          {/* Name */}
+          <input
+            type="text"
+            className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={project.name}
+            onChange={handleInputChange(index, 'name')}
+            placeholder="Enter the name here"
+          />
+
+          {/* Description */}
+          <input
+            type="text"
+            className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={project.description}
+            onChange={handleInputChange(index, 'description')}
+            placeholder="Enter short description here"
+          />
+
+          <input
+            type="text"
+            className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={project.github}
+            onChange={handleInputChange(index, 'github')}
+            placeholder="Enter the GitHub link here"
+          />
+
+          {/* GitHub and Start Date */}
+          <div className="flex flex-row space-x-4">
+            <input
+              type="date"
+              className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={project.startDate}
+              onChange={handleInputChange(index, 'startDate')}
+            />
+
+            {/* End Date */}
+            <input
+              type="date"
+              className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={project.endDate}
+              onChange={handleInputChange(index, 'endDate')}
+            />
+          </div>
+
+          {/* Delete Button */}
+          {localData.length > 1 && (
+            <button
+              onClick={() => handleDelete(index)}
+              className="w-full mt-2 p-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+            >
+              Delete
+            </button>
+          )}
+          {/* Add Button */}
+
+          {index === localData.length - 1 &&
+            (localData[index].description &&
+              localData[index].name &&
+              localData[index].github) != '' && (
+              <button
+                onClick={handleSubmit}
+                className="w-full mt-4 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              >
+                Add New Project
+              </button>
+            )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Projects;
