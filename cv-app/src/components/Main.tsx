d2174232847dd9c21dcab0da8cb34b558e6f82c9
janiopi <janio072003@hotmail.com>
2025-05-18 14:44:43 -0500

    Add: Main component

--- cv-app/src/components/Main.tsx
+++ cv-app/src/components/Main.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import AboutMe from '@/components/AboutMe';
-import Display from '@/components/Display';
-import Experience from '@/components/Experience';
+import AboutMe from '@/components/AboutMe/AboutMe';
+
+import Experience from '@/components/Experience/Experience';
 import { useState } from 'react';
+import Projects from './Projects/Projects';
+import { DM_Sans } from 'next/font/google';
+import AboutMeDisplay from './AboutMe/AboutMeDisplay';
+import ProjectsDisplay from './Projects/ProjectsDisplay';
+import ExperienceDisplay from './Experience/ExperienceDisplay';
 
 function Main() {
   const [personalInfo, setPersonalInfo] = useState({
@@ -14,30 +19,49 @@ function Main() {
     github: '',
     linkedin: '',
   });
-  const [experience, setExperience] = useState({
-    company: '',
-    position: '',
+  const [experience, setExperience] = useState([
+    {
+      //array of experience
+      company: '',
+      position: '',
+      startDate: '',
+      endDate: '',
+    },
+  ]);
+  const [projects, setProjects] = useState([
+    {
+      //array of projects
+      name: '',
+      description: '',
+      github: '',
+      startDate: '',
+      endDate: '',
+    },
+  ]);
+
+  const [education, setEducation] = useState({
+    school: '',
+    degree: '',
     startDate: '',
     endDate: '',
   });
+  // const [skills, setSkills] = useState({
 
   return (
     <div className="flex flex-row h-screen">
       {/* Left Column */}
 
-      <div className="flex-1 bg-gray-500 p-4 h-full border border-red-500">
+      <div className="flex-1 bg-gray-500 p-4 h-full border border-red-500 overflow-y-auto">
         <AboutMe data={personalInfo} setData={setPersonalInfo} />
         <Experience data={experience} setData={setExperience} />
+        <Projects data={projects} setData={setProjects} />
       </div>
 
       {/* Right Column */}
-      <div className="flex-1 bg-gray-500 p-4 h-full border border-red-500">
-        <Display
-          personalData={personalInfo}
-          data={experience}
-          setPersonalData={setPersonalInfo}
-          setData={setExperience}
-        />
+      <div className="flex-1 bg-gray-500 p-4 h-full border border-red-500 overflow-y-auto">
+        <AboutMeDisplay data={personalInfo}></AboutMeDisplay>
+        <ProjectsDisplay data={projects}></ProjectsDisplay>
+        <ExperienceDisplay data={experience}></ExperienceDisplay>
       </div>
     </div>
   );
